c5334820ce8948f9d4495e24fae8c5f1fa21b7f7
Bhuvan <bhuvaneshwarmarri@gmail.com>
2023-12-20 17:16:52 +0530

    seconnd commit

--- factorial.java
+++ factorial.java
@@ -8,6 +8,7 @@ public class factorial {
         long k=s.nextInt();
         for(int i=1;i<=k;i++)
         fact=((fact%1000000007)*(i%1000000007))%1000000007;
+        System.out.println("Helloo");
         System.out.println(fact);
     }    
 }
